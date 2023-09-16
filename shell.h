236525648de956ae9b4c63ce68ee93a53b227920
Oigbochie Blessed <eprince1800@gmail.com>
2023-09-16 20:24:33 +0100

    new commit

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "shell.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+/* #include <sys/wait.h> */
+#include <sys/types.h>
+#include <unistd.h>
+
+ssize_t cat(const char *name);
+
+
+#endif/* SHELL_H */
